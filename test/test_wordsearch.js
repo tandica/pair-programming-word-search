aee4e31119c0e1b2e4caf9ad7d925cc25c9dd9d6
tandica <williams.tandica@gmail.com>
2021-05-05 17:33:54 -0400

    Added new tests

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -35,4 +35,26 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+
+  it("should return true if the word is vertical", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'ARRY')
+
+    assert.isTrue(result);
+  });
+
+  it("should return false if passed an empty array", function() {
+    const result = wordSearch([], 'CAT');
+
+    assert.isFalse(result);
+  });
 });
